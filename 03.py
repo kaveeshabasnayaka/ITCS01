234917bf2461cde4bb4a32d856b58d6ce35349f6
kaveeshabasnayaka <kaveeshabasnayaka9@gmail.com>
2024-03-13 14:55:03 +0530

    python prac new

---
+++ 03.py
@@ -0,0 +1,21 @@
+list1=["apple","mango","banana"]
+print(list1)
+
+print()
+
+print(type(list1))
+
+for x in list1:
+    print(x)
+
+print()
+
+list1[2]="watermelon"
+print(list1)
+
+print(len(list1))
+
+print(list1.pop())
+print(list1)
+list1.insert(1,"strawberry")
+print(list1)
